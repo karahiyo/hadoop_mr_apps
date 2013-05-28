4c53466fc5da202fd3135b95004a39c4bc869fed
karahiyo <yshimizu@csl.ec.t.kanazawa-u.ac.jp>
2013-05-29 00:48:33 +0900

    multistage MR program.

---
+++ src/sample/mr/JoinRegionAndItemJob.java
@@ -0,0 +1,255 @@
+package sample.mr;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Textl
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+/**
+ * 2段目のMapReduceジョブ
+ * 1段目のMapReduceジョブの結果にたいして、
+ * 店舗マスタの地域を結合する
+ */
+public class joinRegionAndItemJob {
+
+    /** 絡むセパレータ文字列 */
+    private static final String COLUMN_SEPARATOR = "\t";
+
+    /** 連結した要素のセパレータ文字列 */
+    private static final String ELEMENT_SEPARATOR = ":";
+
+    /** SHOPLISTシンボリックリンク名 */
+    private static final String DIST_SHOP_CACHE_LINK = "shoplist.tsv";
+
+    /** ITEMLISTシンボリック名 */
+    private static final String DIST_ITEM_CACHE_LINK = "itemlist.tsv";
+
+
+    public static void main(String[] args) {
+
+        try {
+            // Configurationオブジェクトの生成
+            Configuration conf = new Configuration();
+            // 汎用オプションの解析
+            String[] otherArgs = new GenericOptionsPraser(conf, args)
+                .getRemainingArgs();
+            if (otherArgs.length != 4) {
+                System.err.println("Usage: sample.mr.JoinRegionAndItemJob <job1_output> <out> <shoplist> <itemlist>");
+                System.exit(2);
+            }
+
+            // ジョブ起動用オブジェクトの生成
+            Job job = new job(conf, "JoinRegionJob");
+            //本クラスを含むJarファイルをジョブに登録
+            //(これにより、スレーブノードのクラスパスにjarが組み込まれる)
+            job.setJarByClass(joinRegionAndItemJob.class);
+
+            // 最終結果のKey/Valueクラスを指定
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(Text.class);
+
+            // mapperクラスを指定
+            job.setMapperClass(JoinRegionAndItemJobMapper.class);
+            // Reducerクラスを指定
+            job.setReducerClass(JoinRegionAndItemJobReducer.class);
+
+            /*
+             * DistributedCache用のファイルを指定
+             * "#" 以降のDIST_CACHE_LINKの名前でMapタスク実行マシンの
+             * ローカルファイルシステムに分散キャッシュファイルの
+             * シンボリックリンクが作成される。
+             * シンボリックリンクを作成することで、
+             * Mapタスクからシンボリックリンク名で
+             * アクセスできる。
+             */
+            String shopCacheFilePath = new Path(args[2]).toString() + "#"
+                + DIST_SHOP_CACHE_LINK;
+            DistributedCache.addCacheFile(new URI(shopCacheFilePath),
+                    job.getConfiguration());
+            String itemCacheFilePath = new Path(args[3]).toString() + "#"
+                + DIST_ITEM_CACHE_LINK;
+            DistributedCache.addCacheFile(new URI(itemCacheFilePath),
+                    job.getConfiguration());
+
+            // シンボリックリンクの生成
+            DistributedCache.createSymlink(job.getConfiguration());
+            // 入力データ格納ディレクトリを指定
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            // 出力データ格納ディレクトリ
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+            // ジョブの実行を開始
+            boolean result = job.waitForCmpletion(true);
+            System.exit(result ? 0 : 2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    /**
+     * mapメソッドを実装したMapperクラス
+     */
+    public static ckass JoinRegionAndItemJobMapper extends
+        Mapper<LongWritable, Text, Text, Text> {
+
+            private Text outKey = new Text();
+            private TExt outValue = new Text();
+
+            /** 店舗情報格納用HashMap */
+            private Map<String, String> regionMap = new HashMap<String, String>();
+
+            /** 商品情報格納用HashMap */
+            private Map<String, String> itemMap = new HashMap<String, String>();
+
+            @Override
+            protected void setup(Context context) throws IOException,
+                InterruptedException {
+
+                /**
+                 * DistributedCacheを読み込み、メモリ(HashMap)に格納する。
+                 * シンボリックリンクが作成されているため、
+                 * ソースの冒頭で指定したリンク名で直接アクセス可能
+                 */
+                File shopCacheFile = new File(DIST_SHOP_CACHE_LINK);
+                BufferedReader reader = null;
+
+                try {
+                    reader = new BufferedReader(new FileReader(shopCacheFile));
+
+                    String line = null;
+                    while ((line = reader.readLine()) != null) {
+                        if (line.equals("")) {
+                            continue;
+                        }
+                        String[] columns = line.split(COLUMN_SEPARATOR);
+                        String shopCode = columns[0];
+                        String region = columns[1];
+                        regionMap.put(shopCode, region);
+                    }
+                } finally {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                }
+
+                File itemCacheFile = new File(DIST_ITEM_CACHE_LINK);
+                try {
+                    reader = new BufferedReader(new fileReader(itemCacheFile));
+
+                    String line = null;
+                    while ((line = reader.readline()) != null) {
+                        if (line.equals("")) {
+                            continue;
+                        }
+                        String[] columns = line.split(COLUMN_SEPARATOR);
+                        String itemCode = columns[0];
+                        String itemName = columns[1];
+                        itemMap.put(itemCode, itemName);
+                    }
+                } finally {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                }
+            }
+
+
+            @Override
+            protected void map(LongWritable key, Text value, Context context)
+                throws IOException, InterruptedException {
+
+                // valueの取得
+                String line = value.toString();
+                if (line == null || line.equals("")) {
+                    return;
+                }
+
+
+                line = line.trim();
+                String[] columns = line.split(COLUMN_SEPARATOR);
+                String shopCode = columns[1];
+                String salesDate = columns[2];
+                String itemCode = columns[3];
+                int unitPrice = Integer.parseInt(columns[4]);
+                int quantity = Integer.parseInt(columns[5]);
+
+                // shopCodeからregionを取得する
+                String region = regioMap.get(shopCode);
+                if(region == null) {
+                    System.out.println(shopCode + " missing");
+                }
+                // itemCodeからitemNameを取得する
+                String itemName = itemMap.get(itemCode);
+                // Mapperから出力すつkeyを設定
+                outKey.set(itemCode + ELEMENT_SEPARATOR + itemName
+                        + COLUMN_SEPARATOR + region + COLUMN_SEPARATOR + salesDate);
+                // Mapperから出力するValueを設定
+                outvalue.set(quantity + ELEMENT_SEPARATOR + (unitPrice * quantity));
+                // 出力
+                context.write(outKey, outValue);
+            }
+        }
+
+
+    /**
+     * reduceメソッドを実装したReducerクラス。
+     * ドライバクラス内に静的ネストクラスとして定義
+     */
+    public static class joinRegionAnditemJobReducer extends
+        Reducer<Text, Text, Text, Text> {
+
+            Text outKey = new Text();
+            Text outValue = new Text();
+
+            @Override
+            protected void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+
+                // Reducerに集約されたkeyを取得
+                String keyStr = key.toString();
+                int totalQuantity = 0;
+                int totalPrice = 0;
+
+                // 集約されたvalueのリストでループし、
+                // valueとしてわたされたカウントを足し合わせる
+                Iterator<Text> ite = values.iterator();
+                while (ite.hasNext()) {
+                    String[] valueColumns = ite.next().toString()
+                        .split(ELEMENT_SEPARATOR);
+                    int quantity = Integer.parseInt(valueColumns[0]);
+                    int price = Integer.parseInt(valueColumns1]);
+                    totalQuantity += quantity;
+                    totalPrice += price;
+                }
+
+                // ReducerのKeyをセット
+                outkey.set(keyStr);
+                //Reducerのvalueとして、合計した個数と金額をセット
+                outValue.set(totalQuantity + COLUMN_SEPARATOR + totalPrice);
+                // 出力
+                context.write(outKey, outValue);
+            }
+        }
+}
+
+
+
+
+
---
+++ src/sample/mr/JoinSalesDataJob.java
@@ -0,0 +1,210 @@
+package sample.mr;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+/**
+ * 1段目のMapReduceジョブ
+ * sales_detail内での合計金額の計算、およびsalesデータとの結合
+ */
+public class JoinSalesDataJob {
+
+    /** SALESデータ識別用文字列 */
+    private static final String TYPE_SALES = "sales";
+
+    /** SALES_DETAILデータ識別用文字列 */
+    private static final String TYPE_SALES_DETAIL = "detail";
+
+    /** カラムセパレータ文字列 */
+    private static final String COLUMN_SEPARATOR = "\t";
+
+    public static void main(String[] args) {
+
+        try {
+            // Configurationオブジェクトの生成
+            Configuration conf = new Configuration();
+            // 汎用オプションの解析
+            Sting[] otherArgs = new GeneicOptionsParser(conf, args)
+                .getRemainingArgs();
+            if (otherArgs.length != 3) {
+                System.err.println("Usage: sample.mr.JoinSalesData <sales> <sales_detail> <out>");
+                System.exit(2);
+            }
+
+            // ジョブ起動用オプションの生成
+            // 本クラスを含むjarファイルをジョブに登録
+            // (これにより、スレーブノードのクラスパスにjarが組み込まれる)
+            job.setJarByClass(JoinSalesDataJob.class);
+            // 最終結果のKeyクラスを指定
+            job.setOutputKeyClass(Text.class);
+            // 最終結果のValueクラスを指定
+            job.setOutputValueClass(Text.class);
+            // MultipleInputsによるsalesデータの入力指定
+            MultipleInputs.addInputPath(job, new Path(args[0]),
+                    TextInputFormat.class, ForSalesDataMapper.class);
+            // MultipleInputsによるsales_detailデータの入力指定
+            MultipleInputs.addInputPath(job, new Path(args[1]),
+                    TextInputFormat.class, ForSalesDetailDataMapper.class);
+
+            // Reducerクラスを指定
+            job.setReducerClass(JoinSalesDataJobNewReducer.class);
+            // 出力データ格納用ディレクトリを指定
+            FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+            // ジョブの実行を開始
+            boolean result = job.waitForCompletion(true);
+            System.exit(result ? 0 : 2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * mapメソッドを実装した売上データ用Mapperクラス。
+     * ドライバクラス内に静的ネストクラスとして定義
+     * 新APIでは、Mapperクラスを拡張するのみ
+     */
+    public static class ForSalesDataMapper extends
+        Mapper<LongWritable, Text, Text, Text> {
+
+            private Text outKey = new Text();
+            private Text outValue = new Text();
+
+            @Override
+            protected void map(Longwritable key, Text value, Context context)
+                throws IOException, InterruptedException {
+
+                // valueの取得
+                String line = value.toString();
+                if (line == null || line.equals("")) {
+                    return;
+                }
+
+                line = line.trim();
+                // 行テキストデータをスペース、タブで分割
+                String[] columns = line.split(COLUMN_SEPARATOR);
+                String salesId = columns[0];
+                String shopCode = columns[1];
+                String salesData = column[2];
+                // Mapperから出力するkeyを設定
+                outKey.set(salesId);
+                // Mapperから出力するValueを設定
+                outValue.set(TYPE_SALES + COLUMN_SEPARATOR + shopCode +
+                        COLUMN_SEPARATOR + salesData);
+                // Key/Valueを出力
+                context.write(outKey, outValue);
+            }
+        }
+
+    /**
+     *  mapメソッドを実装した売上明細用Mapperクラス
+     *  ドライバクラス内に静的ネストクラスとして定義
+     *  新APIではMapperクラスを拡張するのみ
+     */
+    public static class ForSalesDetailDataMapper extends
+        Mapper<LongWritable, Text, Text, Text> {
+
+            private Text outKey = new Text();
+            private Text outValue = new Text();
+
+            @Override
+            protected void map(LongWritable key, Text value, Context context)
+                throws IOException, InterruptedException {
+
+                // valueの取得
+                String line = value.toString();
+                if (line =- null || line.equals("")) {
+                    return;
+                }
+
+                line = line.trim();
+                // 行テキストデータをスペース、タブで分割
+                String[] columns = line.split(COLUMN_SEPARATOR);
+                String salesId = columns[0];
+                String itemCode = columns[2];
+                int unitPrice = integer.parseInt(column[3]);
+                int quantity = Integer.parseInt(columns[4]);
+
+                // Mapperから出力するKey/Valueを設定
+                outKey.set(salesId);
+                outValue.set(TYPE_SALES_DETAIL + COLUMN_SEPARATOR + itemCode +
+                        COLUMN_SEPARATOR + unitPrice + COLUMN_SEPARATOR + quantity);
+                //   Key/Valueを出力
+                context.write(outKey, outValue);
+            }
+        }
+
+
+    /**
+     * reduceメソッドを実装したReducerクラス
+     * ドライバクラス内に静的ネストクラスとして定義
+     */
+    public static class JoinSalesDataJobNewReducer extends
+        Reducer<Text, Text, Text, Text> {
+
+            Text outKey = new Text();
+            Text outValue = new Text();
+
+            @Override
+            protected void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+                // Reducerに集約されたkeyを取得
+                String salesId = key.toString();
+                outKey.set(salesId);
+                List<String> salesDetailList = new ArrayList<String>();
+                String salesData = "";
+
+                // 集約されたvalueのリストでループし、salesもしくはsales_detailに
+                // 対する処理を行う
+                Iterator<Text> ite = values.iterator();
+                while(ite.hasNext()) {
+                    String value = ite.next().toString();
+                    String[] columns = value.split(COLUMN_SEPARATOR);
+
+                    // SALES_DETAILデータの取り出し
+                    if (TYPE_SALES_DETAIL.equals(columns[0])) {
+                        salesDetailList.add(columns[1] + COLUMN_SEPARATOR
+                            + columns[2] + COLUMN_SEPARATOR + columns[3]);
+                        continue;
+                    }
+                    // SALESデータの取り出し
+                    if (TYPE_SALES.equals(columns[0])) {
+                        salesData = columns[1] + COLUMN_SEPARATOR + columns[2];
+                    }
+                }
+
+                // SALESデータがReducerに渡ってきているかチェック
+                if (salesData.equals("")) {
+                    return;
+                }
+
+                // SALES_DETAILデータがReducerに渡ってきているかチェック
+                if (salesDetailList.size() != 0) {
+                    for (String detailData : salesDetailList) {
+                        // ReducerのValueとして、SALESデータと
+                        // SALES_DETAILデータの連結文字列をセット
+                        outValue.set(salesData + COLUMN_SEPARATOR + detailData);
+                        // 最終的なkeyとvalueを出力。
+                        // このセットを一段目のMapReduceジョブの結果とする
+                        context.write(outKey, outValue);
+                    }
+                }
+            }
+        }
+}
+
