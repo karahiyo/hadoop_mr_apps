2af35a7bcdc84a8aa798f97d6f8ce1f33533af1b
karahiyo <yshimizu@csl.ec.t.kanazawa-u.ac.jp>
2013-05-29 15:47:36 +0900

    Updated WordCountMapperTest.

---
+++ src/test/sample/mr/WordCountMapperTest.java
@@ -0,0 +1,45 @@
+package test.sample.mr;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+import sample.mr.WordCount.WordCountMapper;
+
+public class WordCountMapperTest {
+
+    MapDriver<LongWritable, Text, Text, IntWritable> mapDriver;
+
+    @Before
+    /**
+     * 初期化処理用メソッド
+     */
+    public void setup() {
+        // Mapperオブジェクトを生成
+        WordCountMapper mapper = new WordCountMapper();
+        // テストドライバ生成
+        mapDriver = new MapDriver<LongWritable, Text, Text, IntWritable>();
+        mapDriver.setMapper(mapper);
+    }
+
+    @Test
+    /**
+     * Mapperテストケース
+     */
+    public void testMapper() {
+        // mapメソッドへの入力
+        mapDriver.withInput(new LongWritable(0), new Text(
+                "hadoop hive pig map reduce map reduce hbase hive hive "));
+        // mapメソッドの出力結果の検証
+        mapDriver.withOutput(new Text("hadoop"), new IntWritable(1));
+        mapDriver.withOutput(new Text("hive"), new IntWritable(3));
+        mapDriver.withOutput(new Text("pig"), new IntWritable(1));
+        mapDriver.withOutput(new Text("map"), new IntWritable(1));
+        mapDriver.withOutput(new Text("reduce"), new IntWritable(2));
+        mapDriver.withOutput(new Text("hbase"), new IntWritable(1));
+        mapDriver.runTest();
+    }
+}
